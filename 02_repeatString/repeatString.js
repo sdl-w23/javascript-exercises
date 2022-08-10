cf7af3289cf0ac1bc7df1a9bd5cedc6f74a981eb
sdl-w23 <69322499+sdl-w23@users.noreply.github.com>
2022-08-09 21:33:17 -0500

    Add function that repeats a word
    The function implemented creates a string from a word repeated a specified amount of times.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    let finalString = '';
+    if (times < 0) {
+        return 'ERROR';
+    }
+    for (let i = 0; i < times; i++) {
+        finalString = finalString + word;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
